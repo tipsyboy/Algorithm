4231294b7060ef46b5a84fc91ff5607c92a0cb5e
TipsyBoy <gudah13@naver.com>
2020-12-22 18:39:16 +0900

    lv01_문자열 내 마음대로 정렬하기

---
+++ Programmers/step/01_level1/문자열내마음대로정렬하기.py
@@ -0,0 +1,9 @@
+def solution(strings, n):
+    rst = sorted(strings)
+    rst = sorted(rst, key=lambda x: x[n])
+
+    return rst 
+
+
+# solution(["sun", "bed", "car"], 1)
+solution(["abce", "abcd", "cdx"], 2)
