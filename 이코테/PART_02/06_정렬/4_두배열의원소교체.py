3eb0e285d2c73b9a6d0b2d32587f26471a71d561
TipsyBoy <gudah13@naver.com>
2021-04-21 23:28:31 +0900

    edit 06_정렬

--- 이코테/PART_02/06_정렬/4_두배열의원소교체.py
+++ 이코테/PART_02/06_정렬/4_두배열의원소교체.py
@@ -1,17 +1,14 @@
 n, k = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
 
-arr_a = list(map(int, input().split()))
-arr_b = list(map(int, input().split()))
-
-arr_a = sorted(arr_a)
-arr_b = sorted(arr_b, reverse=True)
+a.sort()
+b.sort(reverse=True)
 
 for i in range(k):
-    if arr_a[i] < arr_b[i]:
-        arr_a[i], arr_b[i] = arr_b[i], arr_a[i]
+    if a[i] < b[i]:
+        a[i], b[i] = b[i], a[i]
     else:
         break
-        # 최대 k번까지 바꾸겠지만 현재 항에서 오름차순으로 정렬된 A가 이미 크다면 이후는 판별 하지 않아도 됨.
 
-print(arr_a)
-print(sum(arr_a))
+print(sum(a))
