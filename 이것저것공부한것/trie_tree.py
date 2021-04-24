311bbe68effe3f46664c78c30c0d31d65bd10c41
TipsyBoy <gudah13@naver.com>
2021-04-24 16:34:54 +0900

    잡다한 주제_ Trie Tree class 구현

---
+++ 이것저것공부한것/trie_tree.py
@@ -0,0 +1,45 @@
+# Node class
+class Node:
+    def __init__(self, key, data=None):
+        self.key = key
+        self.data = data
+        self.children = {}
+
+
+# Trie class
+class Trie:
+    def __init__(self):
+        self.head = Node(None)
+
+    def insert(self, string):
+        current_node = self.head  # Node pointer를 head로 설정
+
+        for char in string:
+            if char not in current_node.children:
+                current_node.children[char] = Node(char)
+            current_node = current_node.children[char]  # Node 포인터를 이동
+
+        current_node.data = string  # 단어를 저장함.
+
+    def search(self, string):
+        current_node = self.head
+
+        for char in string:
+            if char in current_node:
+                current_node = current_node.children[char]
+            else:
+                return False
+
+        if current_node.data:
+            return True
+
+
+trie = Trie()  # 객체 생성
+words = ["frodo", "front", "firefox", "fire"]
+
+for word in words:
+    trie.insert(word)
+
+print(trie.search("friend"))
+print(trie.search("frodo"))
+print(trie.search("fire"))
