edca273ecec9fdd3949b04ac078dccecadfdc972
TipsyBoy <gudah13@naver.com>
2021-05-02 04:23:28 +0900

    19_1012_유기농 배추 dfs구현 추가

--- BOJ/class/class03/19_1012_유기농배추.py
+++ BOJ/class/class03/19_1012_유기농배추.py
@@ -84,3 +84,47 @@ for _ in range(t):
         count += 1  # 배추 지렁이 +1
 
     print(count)
+
+
+# # 3) dfs 구현 - 아오 헷갈려..
+
+# import sys
+# sys.setrecursionlimit(100000)  # 재귀 뎁스
+# input = sys.stdin.readline
+
+# dx = [-1, 1, 0, 0]
+# dy = [0, 0, -1, 1]
+
+
+# def dfs(board, x, y):
+#     for i in range(4):
+#         nx = x + dx[i]
+#         ny = y + dy[i]
+
+#         if nx >= 0 and nx < n and ny >= 0 and ny < m:
+#             if board[nx][ny] == 1:
+#                 board[nx][ny] = -1
+#                 dfs(board, nx, ny)
+
+
+# t = int(input())  # 테스트 케이스
+# for _ in range(t):
+#     m, n, k = map(int, input().split())  # 가 / 세 / 배추 수
+#     board = [[0] * m for _ in range(n)]
+#     count = 0
+
+#     # 배추 위치
+#     for _ in range(k):
+#         a, b = map(int, input().split())
+
+#         board[b][a] = 1
+
+#     # 행렬 탐색
+#     for i in range(n):
+#         for j in range(m):
+#             if board[i][j] == 1:
+#                 board[i][j] = -1
+#                 dfs(board, i, j)
+#                 count += 1
+
+#     print(count)
