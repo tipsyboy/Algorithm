262fb5a04c75366ee61d440a01edc5cd7ed99ab1
TipsyBoy <gudah13@naver.com>
2021-05-03 23:41:39 +0900

    이것저것 공부하기_ 이항계수 edit

--- 이것저것공부한것/이항계수1.py
+++ 이것저것공부한것/이항계수1.py
@@ -46,3 +46,27 @@ print(bino_coef(9, 3))
 
 # python에서의 2차원 배열
 # cache = [[0 for _ in range(3)] for _ in range(4)]
+
+
+# 3. pick 하는 방법
+def bino_coef(n, k):
+    if k > n:
+        return 0
+
+    cache = [[-1 for _ in range(n + 1)] for _ in range(n + 1)]
+
+    def choose(times, got):
+        if times == n:
+            return got == k
+
+        if cache[times][got] != -1:
+            return cache[times][got]
+
+        cache[times][got] = choose(times + 1, got) + choose(times + 1, got + 1)
+        # print(cache)
+        return cache[times][got]
+
+    return choose(0, 0)
+
+
+print(bino_coef(100, 80))
