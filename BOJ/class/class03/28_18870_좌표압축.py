98756ee3c0bfd316fa6bc4fe82137af55e7bcf4c
TipsyBoy <gudah13@naver.com>
2021-05-05 19:09:49 +0900

    [BOJ] class03_28_18870_좌표 압축

--- BOJ/class/class03/28_18870_좌표압축.py
+++ BOJ/class/class03/28_18870_좌표압축.py
@@ -7,8 +7,10 @@ numbers = list(map(int, input().split()))
 numbers_elem = sorted(list(set(numbers)))
 numbers_dict = dict()
 
+# dict
 for i in range(len(numbers_elem)):
     numbers_dict[numbers_elem[i]] = i
 
+# 출력
 for num in numbers:
     print(numbers_dict[num], end=" ")
