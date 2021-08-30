17f15c0ace798a6a04ff46a6196ff897aa221cea
TipsyBoy <gudah13@naver.com>
2021-08-31 01:16:52 +0900

    EDIT PART2 구현 게임 개발

--- 이코테/PART_02/04_구현/4_게임개발.py
+++ 이코테/PART_02/04_구현/4_게임개발.py
@@ -1,59 +1,58 @@
-# 1단계 왼쪽으로 방향 틀기 [북(0)->서(3)->남(2)->동(1)] 순서
-def turn_left():
-    global direction
-    direction -= 1
-    if direction == -1:
-        direction = 3
+import sys
+
+input = sys.stdin.readline
 
 
-n, m = map(int, input().split())  # n*m 맵 생성
-x, y, direction = map(int, input().split())  # 캐릭터의 현재 위치(x, y)와 바라보고 있는 방향(direction)
+n, m = map(int, input().split())
+x, y, direction = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
 
-# game_map과 visit_map 생성
-visit_map = [[0] * m for i in range(n)]
-game_map = []
-for i in range(n):
-    temp = list(map(int, input().split()))
-    game_map.append(temp)
-
-visit_map[x][y] = 1  # 시작 지점 방문
-count = 1  # 다른 곳을 방문한 횟수
-turn_time = 0  # 회전 횟수
-
-# 좌표 이동 방향 [북, 동, 남, 서]
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
+graph[x][y] = 1
+count = 1
+visited = [[False] * m for _ in range(n)]  # 방문배열
 
 while True:
-    turn_left()  # 왼쪽으로 한번 돌아서
-    nx = x + dx[direction]  # 다음에 가야할 방향의 좌표를 구한다.
-    ny = y + dy[direction]  # (next_x, next_y)
+    move = False
 
-    # nx, ny를 검증
-    if visit_map[nx][ny] == 0 and game_map[nx][ny] == 0:  # 갈 수 있는 곳이다 (땅이다 and 방문 안했음)
-        visit_map[nx][ny] = 1
-        x, y = nx, ny
-        count += 1
-        turn_time = 0
-        continue
-    else:
-        turn_time += 1
+    for _ in range(4):
+        if direction == 0:
+            direction = 3
+        else:
+            direction -= 1
 
-    if turn_time == 4:
+        nx = x + dx[direction]
+        ny = y + dy[direction]
+
+        if 0 <= nx < n and 0 <= ny < n:
+            if not visited[nx][ny] and graph[nx][ny] != 1:
+                x, y = nx, ny
+                visited[nx][ny] = True
+                count += 1
+                move = True
+                break
+
+    # 이동 불가인 경우 - 뒤에 바다만 없다면 이동한다.(방문했어도)
+    if not move:
         nx = x - dx[direction]
         ny = y - dy[direction]
-        # 뒤로 갈 수 있으면 뒤로 이동한다. - 이미 방문했더라도
-        if game_map[nx][ny] == 0:
-            x = nx
-            y = ny
+
+        if graph[nx][ny] != 1:
+            x, y = nx, ny
         else:
             break
-        turn_time = 0
 
-print(game_map)
-print(visit_map)
 print(count)
 
 
-# 방문 map과 실제 game map을 따로 두어야 하는 이유
+"""
+PART2 구현. 게임 개발 
+
+    - 이동 불가인 경우 뒤로 가는 경우에서 헷갈렸다.
+      이미 이동불가인 경우는 뒤로도 못간다고 생각했는데,
+      단순히 캐릭터가 방문할 수 있는 최대 수만 세는거라 이미 방문한 곳이라도 뒤에 바다만 없으면 뒤로 이동해서 재탐색한다. 
+
+      대신에 위처럼 이동한 경우 이미 탐색한 공간이기 때문에 count는 하지 않는다. 
+"""
