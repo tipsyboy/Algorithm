1128733718f0662c6c17b6e3309a0e05fd5b7c66
TipsyBoy <gudah13@naver.com>
2021-08-30 23:21:05 +0900

    EDIT PART2 구현 상하좌우

--- 이코테/PART_02/04_구현/1_상하좌우.py
+++ 이코테/PART_02/04_구현/1_상하좌우.py
@@ -1,51 +1,25 @@
 import sys
 
-
-def solution1(n, plans):
-    x, y = 1, 1
-    for plan in plans:
-        if plan == "U":
-            if x < 2:
-                continue
-            x -= 1
-        elif plan == "D":
-            if x > n - 1:
-                continue
-            x += 1
-        elif plan == "L":
-            if y < 2:
-                continue
-            y -= 1
-        elif plan == "R":
-            if y > n - 1:
-                continue
-            y += 1
-
-    print(x, y)
+input = sys.stdin.readline
 
 
-def solution2(n, plans):
-    x, y = 1, 1
-    dx = [0, 0, -1, 1]
-    dy = [-1, 1, 0, 0]
-    move_types = ["L", "R", "U", "D"]
+n = int(input())
+command = list(map(str, input().split()))
 
-    for plan in plans:
-        for i in range(len(move_types)):
-            if plan == move_types[i]:
-                nx = x + dx[i]
-                ny = y + dy[i]
-                break
+x, y = 1, 1
 
-        if nx < 1 or nx > n or ny < 1 or ny > n:
-            continue
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+moves = ["L", "R", "U", "D"]
 
-        x, y = nx, ny
+for com in command:
+    for i in range(4):
+        if com == moves[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-    print(x, y)
+    if 1 <= nx < n + 1 and 1 <= ny < n + 1:
+        x = nx
+        y = ny
 
-
-n = int(sys.stdin.readline())
-plans = input().split()
-solution1(n, plans)
-solution2(n, plans)
+print(x, y)
