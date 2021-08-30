7bfedf7a25bbffcaa4de3e3b65a0c1cdea7ecee3
TipsyBoy <gudah13@naver.com>
2021-08-31 01:27:26 +0900

    EDIT PART3 08. 문자열 재정렬

--- 이코테/PART_03/08_문자열재정렬.py
+++ 이코테/PART_03/08_문자열재정렬.py
@@ -1,16 +1,29 @@
-input_str = input()
-result = []
+import sys
+
+input = sys.stdin.readline
+
+
+input_string = input().rstrip()
+char_list = []
 sum_value = 0
+count = 0  # 숫자가 등장한 횟수
 
-for s in input_str:
-    if s.isalpha():
-        result.append(s)
+for char in input_string:
+    if char.isalpha():
+        char_list.append(char)
     else:
-        sum_value += int(s)
+        count += 1
+        sum_value += int(char)
 
-result.sort()
+char_list.sort()
 
-if sum_value:
-    result.append(str(sum_value))
+if count != 0:
+    char_list.append(str(sum_value))
 
-print("".join(result))
+print("".join(char_list))
+
+"""
+08. 문자열 재정렬
+    - 이 문제의 경우 숫자가 0~9로 구성되어 있는데, 0만 등장한 경우가 있을 수 있어서 
+      숫자가 등장한 횟수를 count로 세주는 것으로 변경해서 다시 작성했다.
+"""
