f9960975e012276ac06820bf0522ae9e8a346a15
TipsyBoy <gudah13@naver.com>
2021-08-30 23:36:21 +0900

    EDIT PART2 구현_왕실의 나이트

--- 이코테/PART_02/04_구현/3_왕실의나이트.py
+++ 이코테/PART_02/04_구현/3_왕실의나이트.py
@@ -1,16 +1,19 @@
-# 현재 나이트의 위치
-coord_val = input()
-x, y = int(coord_val[1]), (ord(coord_val[0]) - ord("a") + 1)
+import sys
 
-# 나이트가 갈 수 있는 방향
-steps = [(-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2), (2, -1), (2, 1)]
-count = 0  # 나아갈 수 있는 방향의 가짓 수
+input = sys.stdin.readline
 
-for step in steps:
-    nx = x + step[0]
-    ny = y + step[1]
+knight = input().rstrip()
+x = int(knight[1]) - 1
+y = ord(knight[0]) - ord("a")
 
-    if nx >= 1 and nx <= 8 and ny >= 1 and ny <= 8:
-        count += 1
+steps = [(-2, -1), (-2, 1), (-1, -2), (1, -2), (-1, 2), (1, 2), (2, -1), (2, 1)]
+rst = 0
 
-print(count)
+for i in range(8):
+    nx = x + steps[i][0]
+    ny = y + steps[i][1]
+
+    if 0 <= nx < 8 and 0 <= ny < 8:
+        rst += 1
+
+print(rst)
