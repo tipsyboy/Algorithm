e5d1f3cb12a974de31bf389dad48389f1c8eedc1
TipsyBoy <gudah13@naver.com>
2021-09-09 16:45:37 +0900

    EDIT PART3 22. 블록 이동하기

--- 이코테/PART_03/26_카드정렬하기.py
+++ 이코테/PART_03/26_카드정렬하기.py
@@ -1,22 +1,22 @@
 import sys
-import heapq
+from heapq import heappush, heappop
 
 input = sys.stdin.readline
 
-n = int(input())
-numbers = []
 
+n = int(input())
+
+pq = []
 for _ in range(n):
-    heapq.heappush(numbers, int(input()))
+    heappush(pq, int(input()))
 
 rst = 0
-while len(numbers) != 1:
-    one = heapq.heappop(numbers)
-    two = heapq.heappop(numbers)
-
-    temp = one + two
+while len(pq) > 1:
+    a = heappop(pq)
+    b = heappop(pq)
+    temp = a + b
     rst += temp
-    heapq.heappush(numbers, temp)
+    heappush(pq, temp)
 
 print(rst)
 
