600354bf4a5e349c952f38f217495e47c5d5b962
TipsyBoy <gudah13@naver.com>
2021-09-16 14:51:48 +0900

    EDIT PART2 미래도시

--- 이코테/PART_02/09_최단 경로/2_미래도시.py
+++ 이코테/PART_02/09_최단 경로/2_미래도시.py
@@ -1,37 +1,87 @@
-#
+import sys
+from heapq import heappush, heappop
+
+input = sys.stdin.readline
 INF = int(1e9)
 
-# node, edge
-n, m = map(int, input().split())
 
-# graph
-graph = [[INF] * (n + 1) for i in range(n + 1)]
+def dijkstra(start, n, graph):
+    dist = [INF] * (n + 1)
+    dist[start] = 0
+    pq = []
+    heappush(pq, (0, start))
 
-# 자신으로 가는 거리
-for i in range(n + 1):
-    for j in range(n + 1):
-        if i == j:
-            graph[i][j] = 0
+    while pq:
+        now_cost, now_node = heappop(pq)
 
-# 그래프 간선 연결
-for i in range(m):
-    a, b = map(int, input().split())
-    graph[a][b] = 1
-    graph[b][a] = 1  # 모든 노드 간의 거리 비용은 1로 동일하다.
+        if now_cost > dist[now_node]:
+            continue
 
-x, k = map(int, input().split())  # x: 도착지, k: 경유지
+        for adj in graph[now_node]:
+            if now_cost + 1 < dist[adj]:
+                dist[adj] = now_cost + 1
+                heappush(pq, (now_cost + 1, adj))
 
-# Floyd-warshall
-for via in range(1, n + 1):
-    for start in range(1, n + 1):
-        for to in range(1, n + 1):
-            graph[start][to] = min(graph[start][to], graph[start][via] + graph[via][to])
+    return dist
 
-distance = graph[1][k] + graph[k][x]
 
-#
-print()
-if distance >= INF:
-    print("-1")
-else:
-    print(distance)
+def solution_dijkstra():
+    n, m = map(int, input().split())  # node, edge
+    graph = [[] for _ in range(n + 1)]
+
+    for _ in range(m):
+        a, b = map(int, input().split())
+
+        # undirected graph
+        graph[a].append(b)
+        graph[b].append(a)
+
+    x, k = map(int, input().split())  # 도착지, 경유지
+
+    answer = 0
+
+    # 1 -> k
+    dist = dijkstra(1, n, graph)
+    answer += dist[k]
+
+    # k -> x
+    dist = dijkstra(k, n, graph)
+    answer += dist[x]
+
+    if answer >= INF:
+        return -1
+
+    return answer
+
+
+def floyd_warshall():
+    n, m = map(int, input().split())
+    graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        graph[i][i] = 0
+
+    for _ in range(m):
+        a, b = map(int, input().split())
+
+        # undirected graph, cost == 1
+        graph[a][b] = 1
+        graph[b][a] = 1
+
+    x, k = map(int, input().split())  # 도착지 경유지
+
+    for w in range(1, n + 1):
+        for i in range(1, n + 1):
+            for j in range(1, n + 1):
+                if graph[i][j] > graph[i][w] + graph[w][j]:
+                    graph[i][j] = graph[i][w] + graph[w][j]
+
+    if graph[1][k] + graph[k][x] >= INF:
+        return -1
+
+    return graph[1][k] + graph[k][x]
+
+
+print(solution_dijkstra())
+
+# print(floyd_warshall())
