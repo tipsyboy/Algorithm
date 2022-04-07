06c91972ea171ff4ad51b8e563b9220c50b77918
TipsyBoy <gudah13@naver.com>
2022-04-07 17:21:35 +0900

    다단계 칫솔 판매

---
+++ Programmers/Dev-Matching 2021 상반기/3_다단계 칫솔 판매.py
@@ -0,0 +1,31 @@
+def solution(enroll, referral, seller, amount) -> list:
+    parent = dict()
+    profit = dict()
+
+    for en, ref in zip(enroll, referral):
+        parent[en] = ref
+        profit[en] = 0
+
+    for s, a in zip(seller, amount):
+        money = a * 100
+        now_seller = s
+        while True:
+            if int(money * 0.1) < 1:
+                profit[now_seller] += money
+                break
+
+            profit[now_seller] += money - int(money * 0.1)
+            money = int(money * 0.1)
+            now_seller = parent[now_seller]
+
+            if now_seller == "-":
+                break
+
+    return list(profit.values())
+
+
+enroll = ["john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"]
+referral = ["-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"]
+seller = ["young", "john", "tod", "emily", "mary"]
+amount = [12, 4, 2, 5, 10]
+solution(enroll, referral, seller, amount)
