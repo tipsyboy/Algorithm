9c7567e92dfb8899eed78b8d94596ef411a687e3
TipsyBoy <gudah13@naver.com>
2022-08-12 15:35:01 +0900

    3273 두 수의 합.py

---
+++ BOJ/category/Two Pointer - 투 포인터/3273_두 수의 합.py
@@ -0,0 +1,51 @@
+# https://www.acmicpc.net/problem/3273
+# 2022-08-12 Fri
+
+import sys
+
+input = sys.stdin.readline
+MAXV = 1_000_000
+
+
+def two_pointer(N: int, arr: list, x: int) -> int:
+    sorted_arr = sorted(arr)
+
+    left, right = 0, N - 1
+    cnt = 0
+    while left < right:
+        sumv = sorted_arr[left] + sorted_arr[right]
+
+        if sumv > x:
+            right -= 1
+        elif sumv < x:
+            left += 1
+        else:
+            cnt += 1
+            # 원소는 서로 다른 수
+            right -= 1
+            left += 1
+
+    return cnt
+
+
+def arr_solution(N: int, arr: list, x: int) -> int:
+    nums = [False] * (MAXV + 1)
+    for elem in arr:
+        nums[elem] = True
+
+    right = x // 2 if x % 2 == 0 else x // 2 + 1
+    cnt = 0
+    for i in range(1, right):
+        if x - i > MAXV:
+            continue
+        if nums[i] and nums[x - i]:
+            cnt += 1
+
+    return cnt
+
+
+N = int(input())
+arr = list(map(int, input().split()))
+x = int(input())
+print(arr_solution(N, arr, x))
+print(two_pointer(N, arr, x))
