958294ee241b854126e9069aa1042b176084b8f9
TipsyBoy <gudah13@naver.com>
2023-01-12 01:11:45 +0900

    merge sort commit

---
+++ 탐구 생활/정렬 알고리즘/merge & quick.py
@@ -0,0 +1,40 @@
+def merge_sort(arr: list) -> None:
+    def merge(start: int, end: int):
+        mid = (start + end) // 2
+        left, right = start, mid
+
+        for i in range(start, end):
+            if right == end:
+                temp[i] = arr[left]
+                left += 1
+            elif left == mid:
+                temp[i] = arr[right]
+                right += 1
+            elif arr[left] <= arr[right]:
+                temp[i] = arr[left]
+                left += 1
+            else:
+                temp[i] = arr[right]
+                right += 1
+
+        for i in range(start, end):
+            arr[i] = temp[i]
+
+    def msort(start: int, end: int) -> None:
+        if start + 1 == end:
+            return
+
+        mid = (start + end) // 2
+
+        msort(start, mid)
+        msort(mid, end)
+        merge(start, end)
+
+    temp = [0] * len(arr)
+    msort(0, len(arr))
+
+
+test_arr = [6, -8, 1, 12, 8, 15, 7, -7]
+print("og:", test_arr)
+merge_sort(test_arr)
+print("sorting:", test_arr)
