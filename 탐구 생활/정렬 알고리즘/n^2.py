a703af301b0233e1414bfb5e753448b728bcdb58
TipsyBoy <gudah13@naver.com>
2023-01-12 00:56:19 +0900

    edit directory

--- 탐구 생활/정렬 알고리즘/n^2.py
+++ 탐구 생활/정렬 알고리즘/n^2.py
@@ -30,35 +30,6 @@ def insertion_sort(numbers):
     return temp
 
 
-# 퀵소트
-def quick_sort(numbers, start, end):
-    if start >= end:
-        return
-
-    pivot = start
-    left = start + 1
-    right = end
-
-    while left <= right:
-
-        # left side
-        while left <= end and numbers[pivot] >= numbers[left]:
-            left += 1
-
-        # right side
-        while right > start and numbers[pivot] <= numbers[right]:
-            right -= 1
-
-        # 완전히 엇갈린 경우
-        if left > right:
-            numbers[pivot], numbers[right] = numbers[right], numbers[pivot]
-        else:
-            numbers[left], numbers[right] = numbers[right], numbers[left]
-
-    quick_sort(numbers, start, right - 1)
-    quick_sort(numbers, right + 1, end)
-
-
 numbers = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
 
 # 선택 정렬
@@ -70,10 +41,3 @@ print(f"정렬 후: {selection_sort(numbers)}")
 print("###### 삽입 정렬 ######")
 print(f"정렬 전: {numbers}")
 print(f"정렬 후: {insertion_sort(numbers)}")
-
-# 퀵 소트
-print("###### 퀵 소트 ######")
-copy_numbers = copy.deepcopy(numbers)
-print(f"정렬 전: {copy_numbers}")
-quick_sort(copy_numbers, 0, len(copy_numbers) - 1)
-print(f"정렬 후: {copy_numbers}")
