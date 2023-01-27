185c9de02b61a5d5c4a25be7183e4de44b6dbfd5
TipsyBoy <gudah13@naver.com>
2023-01-28 02:04:03 +0900

    10816_숫자 카드 2.py

--- BOJ/category/Binary Search - 이분탐색/10000-/10816_숫자 카드 2.py
+++ BOJ/category/Binary Search - 이분탐색/10000-/10816_숫자 카드 2.py
@@ -1,60 +1,97 @@
 # https://www.acmicpc.net/problem/10816
 
 import sys
+from collections import Counter
 from bisect import bisect_left, bisect_right
 
 input = sys.stdin.readline
 
 
-def sol1() -> list:
-    ans = []
+def sol1_counter_module() -> list:
+    count_arr = Counter(arr)
 
-    for t in arr:
-        ans.append(str(bisect_right(cards, t) - bisect_left(cards, t)))
+    rst = []
+    for target in target_arr:
+        rst.append(count_arr[target])
 
-    return ans
+    return rst
 
 
-def left_idx(target: int, cards: list) -> int:
-    lo, hi = 0, len(cards)
+def sol2_bisect_module() -> list:
+    arr.sort()
 
-    while lo < hi:
-        mid = (lo + hi) // 2
+    rst = []
+    for target in target_arr:
+        rst.append(bisect_right(arr, target) - bisect_left(arr, target))
 
-        if target <= cards[mid]:
-            hi = mid
+    return rst
+
+
+def sol3_dict() -> list:
+    arr.sort()
+
+    couter = dict()
+    for e in arr:
+        couter[e] = couter.get(e, 0) + 1
+
+    rst = []
+    for target in target_arr:
+        if target in couter:
+            rst.append(couter[target])
         else:
-            lo = mid + 1
+            rst.append(0)
 
-    return lo
-
-
-def right_idx(target: int, cards: list):
-    lo, hi = 0, len(cards)
-
-    while lo < hi:
-        mid = (lo + hi) // 2
-
-        if target < cards[mid]:
-            hi = mid
-        else:
-            lo = mid + 1
-
-    return lo
+    return rst
 
 
-def sol2() -> list:
-    ans = []
-    for t in arr:
-        ans.append(str(right_idx(t, cards) - left_idx(t, cards)))
+def sol4_binary_search() -> list:
+    def left_idx(t: int, arr: list) -> int:
+        lo, hi = 0, len(arr)
 
-    return ans
+        while lo < hi:
+            mid = (lo + hi) // 2
+
+            if arr[mid] >= t:
+                hi = mid
+            else:
+                lo = mid + 1
+
+        return lo
+
+    def right_idx(t: int, arr: list) -> int:
+        lo, hi = 0, len(arr)
+
+        while lo < hi:
+            mid = (lo + hi) // 2
+
+            if arr[mid] > t:
+                hi = mid
+            else:
+                lo = mid + 1
+
+        return lo
+
+    arr.sort()
+    rst = []
+    for target in target_arr:
+        rst.append(right_idx(target, arr) - left_idx(target, arr))
+
+    return rst
 
 
 N = int(input())
-cards = sorted(map(int, input().split()))
-M = int(input())
 arr = list(map(int, input().split()))
+M = int(input())
+target_arr = list(map(int, input().split()))
 
-# print(" ".join(sol1()))
-print(" ".join(sol2()))
+# print(*sol1_counter_module())
+# print(*sol2_bisect_module())
+# print(*sol3_dict())
+print(*sol4_binary_search())
+
+"""
+10816. 숫자 카드 2
+    - 모듈 사용(Counter, bisect), dict 사용, binary_search 직접 구현 4가지 방법으로 품
+
+    - 4번을 구현할 수 있으면서 bisect를 사용하자
+"""
