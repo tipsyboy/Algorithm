624b610c657b539e6af33e27d03af47f2af8f233
TipsyBoy <gudah13@naver.com>
2023-02-07 21:17:41 +0900

    진수 변환기

---
+++ 탐구 생활/진수 변환.py
@@ -0,0 +1,21 @@
+"""
+    - 십진수 -> {base}진수로 변환 
+    
+    - 나머지 String(T)을 정의해놓고 나머지를 붙여 나간다.
+"""
+
+
+def convert_numeral_system(number: int, base: int):
+    T = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    q, r = divmod(number, base)
+    return convert_numeral_system(q, base) + T[r] if q else T[r]
+
+    # if q == 0:
+    #     return T[r]
+    # else:
+    #     return convert_numeral_system(q, base) + T[r]
+
+
+N, B = map(int, input().split())
+print(convert_numeral_system(N, B))
