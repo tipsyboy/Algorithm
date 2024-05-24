c680701201b94d1a5fc16eeea95122a41860a0c9
양형모 <gudah13@naver.com>
2024-05-24 15:23:39 +0900

    16956_늑대와 양

--- BOJ/category/Ad-hoc 애드혹/16956_늑대와 양.py
+++ BOJ/category/Ad-hoc 애드혹/16956_늑대와 양.py
@@ -5,7 +5,6 @@
     - 이게 정해가 맞는지는 모르겠지만 늑대 주변을 몽땅 막았다.
 """
 
-
 import sys
 
 input = sys.stdin.readline
@@ -21,16 +20,16 @@ for i in range(R):
 
         for d in range(4):
             nx, ny = i + directions[d][0], j + directions[d][1]
-            
+
             if nx < 0 or nx >= R or ny < 0 or ny >= C:
                 continue
-            
+
             if grid[nx][ny] == "S":
                 ans = 0
                 break
             if grid[nx][ny] == ".":
                 grid[nx][ny] = "D"
-        
+
     if not ans:
         break
 
