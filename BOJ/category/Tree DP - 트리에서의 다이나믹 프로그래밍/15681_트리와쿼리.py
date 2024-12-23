3cc1cd996d0a1d1487e2977dfbd80a4df6ec3732
tipsyboy <gudah13@naver.com>
2024-12-23 17:19:28 +0900

    15681_트리와쿼리.py

--- BOJ/category/Tree DP - 트리에서의 다이나믹 프로그래밍/15681_트리와쿼리.py
+++ BOJ/category/Tree DP - 트리에서의 다이나믹 프로그래밍/15681_트리와쿼리.py
@@ -1,45 +1,36 @@
+# 2024.11.28 THU
+# https://www.acmicpc.net/problem/15681
+
 import sys
 
+sys.setrecursionlimit(10**5 + 5)
 input = sys.stdin.readline
-sys.setrecursionlimit(10 ** 6)
 
 
-def DFS(now):
-    # if visited[now]: # 왜 필요하지..?
-    #     return dp[now]
+def dfs(cur):
+    visited[cur] = True
 
-    visited[now] = True
-
-    for adj in graph[now]:
-        if visited[adj]:
+    for sub in tree[cur]:
+        if visited[sub]:
             continue
-        dp[now] += DFS(adj)
+        node_cnt[cur] += dfs(sub)
 
-    return dp[now]
+    return node_cnt[cur]
 
 
 N, R, Q = map(int, input().split())
-graph = [[] for _ in range(N + 1)]
+tree = [[] for _ in range(N + 1)]
 for _ in range(N - 1):
     u, v = map(int, input().split())
-
-    # 트리는 무방향 그래프이다.
-    graph[u].append(v)
-    graph[v].append(u)
+    tree[u].append(v)
+    tree[v].append(u)
 
 visited = [False] * (N + 1)
-dp = [1] * (N + 1)  # 정점 i를 루트로하는 서브트리의 정점 개수. 이때 정점 i를 포함하므로 1로 시작한다.
-DFS(R)
+node_cnt = [1] * (N + 1)
+dfs(R)
 
+ans = []
 for _ in range(Q):
-    query = int(input())
-    print(dp[query])
-
-
-"""
-    트리 
-    - 임의의 두 노드 u, v에서 최단 경로는 유일하다.
-    - 임의의 노드 x를 잡고 부모노드와의 간선을 끊으면, 서브트리가 된다. 
-
-    - 트리에서의 정보를 DP로 저장한다 개념
-"""
+    u = int(input())
+    ans.append(node_cnt[u])
+print(*ans, sep="\n")
