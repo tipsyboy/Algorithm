0ec5deff7c19b314294f9218bb5ee2767f1a706d
tipsyboy <gudah13@naver.com>
2025-02-18 15:49:10 +0900

    11058_크리보드.py

---
+++ BOJ/category/DP - 다이나믹 프로그래밍/10000-/11058_크리보드.py
@@ -0,0 +1,15 @@
+# 2025.02.01 SAT
+# https://www.acmicpc.net/problem/11058
+
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+dp = [0] * (N + 1)
+for i in range(1, N + 1):
+    dp[i] = dp[i - 1] + 1
+    for j in range(3, i + 1):
+        dp[i] = max(dp[i], dp[i - j] * (j - 1))
+
+print(dp[N])
